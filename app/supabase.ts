7c878749318b6317ef3e2d8124141bff379d9a19
telkomdev-rasyid <rasyid.ufa@gmail.com>
2025-01-30 15:16:34 +0700

    feat: Add environment variables for Supabase configuration; refactor supabase client initialization

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vbzblzznemqjdbqpenbi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiemJsenpuZW1xamRicXBlbmJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxNzg5MjgsImV4cCI6MjA1Mjc1NDkyOH0.Yt07gc1G-0yp68UWCwINqtvpIRSHJvpNY-OPNbljiDY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
